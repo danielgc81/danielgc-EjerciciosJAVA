bde0ef3b2d2dd4cbe66b3aebe039325528f1ac36
Daniel Garrido <danielgc81@educastur.es>
2024-10-04 09:31:31 +0200

    Mejorando el sistema para no poder poner numeros negtivos ni mayores de 255 en el Ejercicio12.java

--- EjerciciosClase-JAVA/src/Unidad1/Ejercicios/Ejercicio12.java
+++ EjerciciosClase-JAVA/src/Unidad1/Ejercicios/Ejercicio12.java
@@ -7,29 +7,35 @@ public class Ejercicio12 {
 
         System.out.println("Valor del Rojo (0 - 255): ");
         double r = sc.nextDouble();
-        if(r > 255) {
-            System.out.println("\u001B[31mERROR: Valor Incorrecto");
-            System.out.println("Vuelve a introducir un numero correcto\u001B[0m");
-            System.out.println("Valor del Rojo (0 - 255): ");
-            r = sc.nextDouble();
+        if(r > 255 || r < 0) {
+            while(r > 255 || r < 0){
+                System.out.println("\u001B[31mERROR: Valor Incorrecto");
+                System.out.println("Vuelve a introducir un numero correcto\u001B[0m");
+                System.out.println("Valor del Rojo (0 - 255): ");
+                r = sc.nextDouble();
+            }
         }
 
         System.out.println("Valor del Verde (0 - 255): ");
         double g = sc.nextDouble();
-        if(g > 255) {
-            System.out.println("\u001B[31mERROR: Valor Incorrecto");
-            System.out.println("Vuelve a introducir un numero correcto\u001B[0m");
-            System.out.println("Valor del Verde (0 - 255): ");
-            g = sc.nextDouble();
+        if(g > 255 || g < 0) {
+            while(g > 255 || g < 0){
+                System.out.println("\u001B[31mERROR: Valor Incorrecto");
+                System.out.println("Vuelve a introducir un numero correcto\u001B[0m");
+                System.out.println("Valor del Verde (0 - 255): ");
+                g = sc.nextDouble();
+            }
         }
 
         System.out.println("Valor del Azul (0 - 255): ");
         double b = sc.nextDouble();
-        if(b > 255){
-            System.out.println("\u001B[31mERROR: Valor Incorrecto");
-            System.out.println("Vuelve a introducir un numero correcto\u001B[0m");
-            System.out.println("Valor del Azul (0 - 255): ");
-            b = sc.nextDouble();
+        if(b > 255 || b < 0){
+            while(b > 255 || b < 0){
+                System.out.println("\u001B[31mERROR: Valor Incorrecto");
+                System.out.println("Vuelve a introducir un numero correcto\u001B[0m");
+                System.out.println("Valor del Azul (0 - 255): ");
+                b = sc.nextDouble();
+            }
         }
 
         double y = 0.299 * r + 0.587 * g + 0.114 * b;
