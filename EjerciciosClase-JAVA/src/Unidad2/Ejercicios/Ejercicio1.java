ee4e6e5682cafd8aca839c2adea167b55020d266
Daniel Garrido <danielgc81@educastur.es>
2024-10-08 17:12:36 +0200

    Describiendo las variables

--- EjerciciosClase-JAVA/src/Unidad2/Ejercicios/Ejercicio1.java
+++ EjerciciosClase-JAVA/src/Unidad2/Ejercicios/Ejercicio1.java
@@ -5,19 +5,19 @@ public class Ejercicio1 {
     public static void main(String[] args){
         // Escribe un programa que calcule la división exacta de dos números introducidos por teclado. Si el
         // divisor es cero, se abortará el proceso mostrando un mensaje de error
-        double num1;
-        double num2;
+        double dividendo;
+        double divisor;
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Valor a:");
-        num1 = sc.nextDouble();
+        dividendo = sc.nextDouble();
         System.out.println("Valor b:");
-        num2 = sc.nextDouble();
+        divisor = sc.nextDouble();
 
-        if (num2 == 0) {
+        if (divisor == 0) {
             System.out.println("ERROR: El divisor no puede ser cero");
         } else {
-            double division = num1 / num2;
+            double division = dividendo / divisor;
             System.out.printf("La division da %.2f", division);
         }
     }
