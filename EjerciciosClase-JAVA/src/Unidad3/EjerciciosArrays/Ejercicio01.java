d502169d2504189ce06dc67f1485574033c4771b
Daniel Garrido <danielgc81@educastur.es>
2024-11-15 08:51:28 +0100

    Subiendo el ejercicio02.java

--- EjerciciosClase-JAVA/src/Unidad3/EjerciciosArrays/Ejercicio01.java
+++ EjerciciosClase-JAVA/src/Unidad3/EjerciciosArrays/Ejercicio01.java
@@ -1,5 +1,6 @@
 package Unidad3.EjerciciosArrays;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Ejercicio01 {
@@ -8,7 +9,8 @@ public class Ejercicio01 {
       // introducirá por teclado. Antes de finalizar mostrará el número de veces que salió cada una de las caras.
       
       Scanner sc = new Scanner(System.in);
-      
+      Random r = new Random();
+
       while (true) {
          System.out.println("Cuantas veces quieres lanzar el dado: ");
          int nLanazaminetos = sc.nextInt();
@@ -16,10 +18,10 @@ public class Ejercicio01 {
          int[] carasDado = new int[6];
    
          for (int i = 0; i < nLanazaminetos; i++) {
-            int n = (int) (Math.random() * carasDado.length) + 0;
+            int n = r.nextInt(6);
             carasDado[n]++;
          }
-   
+         
          System.out.println("¿Cuantas veces han salido las caras de un dado?: ");
          for (int i = 0; i < carasDado.length; i++) {
             System.out.println(i + " -> " + carasDado[i]);
---
+++ EjerciciosClase-JAVA/src/Unidad3/EjerciciosArrays/Ejercicio02.java
@@ -0,0 +1,48 @@
+package Unidad3.EjerciciosArrays;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Ejercicio02 {
+   public static void main(String[] args) {
+      /*
+      Programa que realice las tareas siguientes:
+         a) Crear un vector de números enteros de un tamaño aleatorio comprendido entre 10 y 50
+            elementos.
+         b) Llenar el vector con números aleatorios comprendidos entre -100 y 100 sin repetir ninguno.
+         c) Crear un segundo vector que contenga los elementos del anterior almacenados en orden inverso.
+         d) Mostrar ambos vectores en la consola.
+      */
+      Random r = new Random();
+      int tamañoArr = r.nextInt(40) + 10;
+
+      // Primer Array
+      int[] arr = new int[tamañoArr];
+      for (int i = 0; i < arr.length; i++) {
+         int nRandom, j;
+         do {
+            nRandom = r.nextInt(200) - 100;
+            j = 0;
+            while (j < i && nRandom != arr[j]) {
+               j++;
+            }
+         } while (j < i);
+         arr[i] = nRandom;
+      }
+
+      for (int i = 0; i < arr.length; i++) {
+         System.out.print(arr[i] + " ");
+      }
+
+      // Segundo Array
+      int[] arr2 = new int[arr.length];
+      System.out.println();
+      for (int i = 0; i < arr2.length; i++) {
+         arr2[i] = arr[(tamañoArr - 1) - i];
+      }
+
+      for (int i = 0; i < arr2.length; i++) {
+         System.out.print(arr2[i] + " ");
+      }
+   }
+}
